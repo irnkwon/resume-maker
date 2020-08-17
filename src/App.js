630fd206353cb144cb482b015ad7ca7d979f4332
irnkwon <haeryun2004@gmail.com>
2020-08-17 16:06:12 -0400

    Add contact

--- src/App.js
+++ src/App.js
@@ -6,19 +6,38 @@ import { Card,
   DatePicker,
   Space,
   Layout,
-  Typography } from 'antd';
+  Typography,
+  Select
+} from 'antd';
+
+import {
+  PhoneFilled,
+  MailFilled,
+  CodeFilled,
+  GithubFilled,
+  LinkedinFilled
+} from '@ant-design/icons';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      name: ""
+      // Basic Information
+      name: "Irene Kwon",
+
+      // Contact
+      phone: "123-456-7890",
+      email: "irene.haeryun.kwon@gmail.com",
+      portfolio: "irnkwon.github.io",
+      github: "github.com/irnkwon",
+      linkedin: "linkedin.com/in/irene-kwon"
     }
   }
 
   handleChange = (e) => {
-    this.setState({ name: e.target.value });
+    const value = e.target.value;
+    this.setState({ [e.target.name]: value });
   }
 
   render() {
@@ -27,45 +46,67 @@ class App extends React.Component {
     const { RangePicker } = DatePicker;
     const { TextArea } = Input;
     const { Sider, Content, Footer } = Layout;
-    const { Title } = Typography;
+    const { Title, Paragraph } = Typography;
+    const { Option } = Select;
+    const selectBefore = (
+      <Select defaultValue="http://" className="select-before">
+        <Option value="http://">http://</Option>
+        <Option value="https://">https://</Option>
+      </Select>
+    );
 
     return (
-      <Layout>
-        <Sider>
-          <Card title="Resume Maker" bordered={true} style={{ width: 350 }}>
+      <Layout className="app-container">
+        <Sider width={350} theme={"light"}>
+          <Card title="Resume Maker" bordered={false}>
             <Collapse defaultActiveKey={['1', '2', '3', '4', '5']} ghost>
               <Panel header="Basic Information" key="1">
-                <Input placeholder="Your Name" onChange={this.handleChange}></Input>
+                <Input name="name" placeholder="Your Name" onChange={this.handleChange}></Input>
               </Panel>
-              <Panel header="Education" key="2">
+              <Panel header="Contact" key="2">
+                <Space direction="vertical" size={12}>
+                  <Input name="phone" placeholder="Phone Number" onChange={this.handleChange}></Input>
+                  <Input name="email" placeholder="Email Address" onChange={this.handleChange}></Input>
+                  <Input 
+                    name="portfolio" 
+                    placeholder="Portfolio Address" 
+                    addonBefore={selectBefore} 
+                    onChange={this.handleChange}
+                  ></Input>
+                  <Input 
+                    name="github" 
+                    placeholder="Github Address" 
+                    addonBefore={selectBefore} 
+                    onChange={this.handleChange}
+                  ></Input>
+                  <Input 
+                    name="linkedin" 
+                    placeholder="LinkedIn Address" 
+                    addonBefore={selectBefore} 
+                    onChange={this.handleChange}
+                  ></Input>
+                </Space>
+              </Panel>
+              <Panel header="Education" key="3">
                 <Space direction="vertical">
-                  <Input placeholder="School"></Input>
-                  <Input placeholder="Location"></Input>
+                  <Input name="school" placeholder="School" onChange={this.handleChange}></Input>
+                  <Input name="schoolLocation" placeholder="Location" onChange={this.handleChange}></Input>
                   <RangePicker bordered={true} />
                   <TextArea placeholder="Education Description"></TextArea>
                 </Space>
               </Panel>
-              <Panel header="Experience" key="3">
+              <Panel header="Experience" key="4">
                 <Space direction="vertical">
-                  <Input placeholder="Company"></Input>
-                  <Input placeholder="Location"></Input>
+                  <Input name="company" placeholder="Company" onChange={this.handleChange}></Input>
+                  <Input name="companyLocation" placeholder="Location" onChange={this.handleChange}></Input>
                   <RangePicker bordered={true} />
                   <TextArea placeholder="Experience Description"></TextArea>
                 </Space>
               </Panel>
-              <Panel header="Projects" key="4">
+              <Panel header="Projects" key="5">
                 <Space direction="vertical">
                   <Input placeholder="Project Name"></Input>
                   <TextArea placeholder="Project Description"></TextArea>
-                </Space>
-              </Panel>
-              <Panel header="Contact" key="5">
-                <Space direction="vertical" size={12}>
-                  <Input placeholder="Phone Number"></Input>
-                  <Input placeholder="Email Address"></Input>
-                  <Input placeholder="Portfolio Address"></Input>
-                  <Input placeholder="Github Address"></Input>
-                  <Input placeholder="LinkedIn Address"></Input>
                 </Space>
               </Panel>
             </Collapse>
@@ -73,9 +114,17 @@ class App extends React.Component {
         </Sider>
         <Layout className="main-content">
           <Content>
-            <Title level={2}>{this.state.name}</Title>
+            <Title level={1}>{this.state.name}</Title>
+            <Paragraph><PhoneFilled /> {this.state.phone}</Paragraph>
+            <Paragraph><a href={"mailto:" + this.state.email}><MailFilled /> {this.state.email}</a></Paragraph>
+            <Paragraph><a href={"http://" + this.state.portfolio} target="_blank" rel="noopener noreferrer">
+              <CodeFilled /> {this.state.portfolio}</a></Paragraph>
+            <Paragraph><a href={"http://" + this.state.github} target="_blank" rel="noopener noreferrer">
+              <GithubFilled /> {this.state.github}</a></Paragraph>
+            <Paragraph><a href={"http://" + this.state.linkedin} target="_blank" rel="noopener noreferrer">
+              <LinkedinFilled /> {this.state.linkedin}</a></Paragraph>
           </Content>
-          <Footer>
+          <Footer className="footer">
             2020 © designed and developed by Irene Kwon
           </Footer>
         </Layout>
