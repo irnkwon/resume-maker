2028e1773fa0b2574801234472fc6380537f6c59
irnkwon <haeryun2004@gmail.com>
2020-08-17 13:18:36 -0400

    Updates

---
+++ precache-manifest.48e34fa9ed4023d6b202f9bf542f7033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f641898ee6dfd108f7ad87f049d20d9",
+    "url": "/resume-maker/index.html"
+  },
+  {
+    "revision": "fad1e6ee1364a6f9996b",
+    "url": "/resume-maker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b07b78d22abffc48ab08",
+    "url": "/resume-maker/static/js/2.78b5dc4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume-maker/static/js/2.78b5dc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad1e6ee1364a6f9996b",
+    "url": "/resume-maker/static/js/main.a7faef58.chunk.js"
+  },
+  {
+    "revision": "f42c4138a2b14de0037b",
+    "url": "/resume-maker/static/js/runtime-main.1d49e761.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume-maker/static/media/logo.5d5d9eef.svg"
+  }
+]);
